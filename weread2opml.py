3e440544fd8abb4b887b480af36816bd342bd9d5
Yu <sniky.hack@gmail.com>
2023-05-15 23:48:54 +0800

    weread2opml

--- weread2opml.py
+++ weread2opml.py
@@ -5,5 +5,8 @@ This is a script to convert WeRead's exported note file to OPML file.
 
 import sys
 
-raw_content = sys.argv[1]
-print(raw_content)
+if len(sys.argv) < 2:
+    print("Usage: python3 weread2opml.py <path/to/weread_exported_file>")
+else:
+  raw_content = sys.argv[1:]
+  print(raw_content)
